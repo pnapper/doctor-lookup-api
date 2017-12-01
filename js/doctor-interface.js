0b6ae88f4723c58d50f841236851639be6adfac2
Patrick Napper <patrick@patricknapper.com>
2017-12-01 09:39:40 -0800

    add initial index.html file and js file with api query url and json parse response jquery promise function.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,92 @@
 var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#symptomDoctor').click(function() {
+    let symptom = $('#symptom').val();
+    $('#symptom').val("");
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.showDoctors').append(`${body.data.practices.name}`+"<br>"+ `${body.data.practices.visit_address}`+"<br>"+`${body.data.practices.phones}`+"<br>"+ `${body.data.practices.website}`+"<br>"+`"Accepting new patients:"${body.data.practices.accepts_new_patients}`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
+
+//using curl and promises .then .fail
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function(response) {
+//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//     }).fail(function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+//     });
+//   });
+// });
+
+// Javascript Method
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//
+//     let request = new XMLHttpRequest();
+//     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//
+//     request.onreadystatechange = function() {
+//       if (this.readyState === 4 && this.status === 200) {
+//         let response = JSON.parse(this.responseText);
+//         getElements(response);
+//       }
+//     }
+//
+//     request.open("GET", url, true);
+//     request.send();
+//
+//     const getElements = function(response) {
+//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//     }
+//   });
+// });
+
+//jQuery option
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.")
+//       }
+//     });
+//   });
+// });

c50cbea5e00b7f8383ab336dc08b3c93a9a6874c
Patrick Napper <patrick@patricknapper.com>
2017-12-01 16:58:07 -0800

    split up front end and back end logic into seperate files. Name search function is not working.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Doctor Lookup
+
+This App is a website where users may enter a medical issue (ie: “sore throat”, "rash", etc.) into a form, submit it, and receive a list of doctors in their city who can treat their medical issue.
+
+## Getting Started
+
+1. Clone the project from https://github.com/pnapper/doctor-lookup-api onto your local machine.
+
+2. Navigate to the project folder in the console and run "npm install" and "bower install".
+
+3. From the console run the application using "gulp serve"
+
+4. Enjoy!
+
+### Prerequisites
+
+Web browser required.
+
+### Installing
+
+From the console run:
+npm install
+bower install
+
+## Versioning
+
+version 1.0
+
+## Authors
+
+* **Patrick Napper**  [Github](https://github.com/pnapper)
+
+## License
+
+MIT Licence
--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,34 +1,63 @@
-var apiKey = require('./../.env').apiKey;
+import {Doctor} from "./../js/doctor.js"
 
-$(document).ready(function() {
-  $('#searchSymptom').click(function() {
-    let symptom = $('#symptom').val();
-    $('#symptom').val("");
+$(document).ready(function(){
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=47.606%2C-122.332%2C100&user_location=47.606%2C-122.332&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let doctor = JSON.parse(response);
-      response.data.forEach(function(doctor) {
-      $('.showDoctors').append(`${doctor.profile.first_name}`+" "+`${doctor.profile.last_name}`+"<br>");
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
+ let doctorObj = new Doctor();
+ doctorObj.getConditions();
+ $(".findDoc").click(function(){
+   let name = $('.doctorName').val();
+   let condition = $('.conditionList').val();
+   try {
+     if(condition == "non"){
+       throw("Please choose a condition");
+     }
+     else{
+       $('.searchPage').addClass('hide');
+       doctorObj.getDoctorList(condition);
+       console.log(doctorObj.getDoctorList(condition));
+     }
+   } catch (e) {
+     $(".searchPage").prepend(`<div class="alert alert-warning alert-dismissable fade in">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    ${e}
+  </div>`);
+  }
+ });
 });
-});
+
+// var apiKey = require('./../.env').apiKey;
+
+//Promise version not working
+// $(document).ready(function() {
+//   $('#searchSymptom').click(function() {
+//     let symptom = $('#symptom').val();
+//     $('#symptom').val("");
+//
+//       let promise = new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=47.606%2C-122.332%2C100&user_location=47.606%2C-122.332&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(Error(request.statusText));
+//         }
+//       }
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//
+//     promise.then(function(response) {
+//       let body = JSON.parse(response);
+//       body.forEach(function(doctor) {
+//       $('.showDoctors').append(`${doctor.profile.first_name}`+" "+`${doctor.profile.last_name}`+"<br>");
+//       });
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+//     });
+//   });
+// });
+
 
 // $(document).ready(function() {
 //   $('#searchSymptom').click(function() {
@@ -51,25 +80,28 @@ $(document).ready(function() {
 //     });
 //   });
 // });
-
-$(document).ready(function() {
-  $('#searchName').click(function() {
-    let name = $('#doctorName').val();
-    $('#doctorName').val("");
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=47.606%2C-122.332%2C100&user_location=47.606%2C-122.332&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        response.data.forEach(function(doctor) {
-        $('.showDoctors').append(`${doctor.profile.first_name}`+" "+`${doctor.profile.last_name}`+"<br>"+ `${doctor.visit_address}`+"<br>"+`${doctor.phones}`+"<br>"+ `${doctor.website}`+"<br>"+`"Accepting new patients:"${doctor.accepts_new_patients}`+"<br>");
-      });
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
-    });
-  });
-});
+//
+// $(document).ready(function() {
+//   $('#searchName').click(function() {
+//     let name = $('#doctorName').val();
+//     $('#doctorName').val("");
+//     $.ajax({
+//       url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=47.606%2C-122.332%2C100&user_location=47.606%2C-122.332&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         response.data.forEach(function(doctor) {
+//         $('.showDoctors').append(`${doctor.profile.first_name}`+" "+`${doctor.profile.last_name};
+          // data.practices.forEach(function(data) {
+          //   $('.showDetail').append( `${doctor.visit_address}`+"<br>"+`${doctor.phones}`+"<br>"+ `${doctor.website}`+"<br>"+`"Accepting new patients:"${doctor.accepts_new_patients}`+"<br>");
+//       });
+        // });
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.")
+//       }
+//     });
+//   });
+// });
